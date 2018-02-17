bc4585e8a01e80a6a11f2b3c8b0042c6d2679cfb
Ii Masamitsu <ii.masamitsu@gmail.com>
2018-02-17 09:30:28 +0900

    test: deckのテストを追加

--- blackjack/deck_test.py
+++ blackjack/deck_test.py
@@ -5,3 +5,10 @@ def test_generating_deck():
     deck = dk.generate_deck()
     assert len(deck) == 52
     assert [c.number for c in deck].count(1) == 4
+
+def test_dealing_card():
+    deck = dk.generate_deck()
+    top_of_deck = deck[0]
+    deck, hand = dk.deal(deck, [])
+    assert len(deck) == 51
+    assert top_of_deck in hand
