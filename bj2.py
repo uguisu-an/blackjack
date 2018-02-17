a8ff40325bb921213884ad8f464bdd5f56d66741
Ii Masamitsu <ii.masamitsu@gmail.com>
2018-02-17 21:31:15 +0900

    refactor: turnに移した

--- bj2.py
+++ bj2.py
@@ -5,35 +5,24 @@ from blackjack.hit_or_stand_component import HitOrStandComponent
 from blackjack.result_component import ResultComponent
 from blackjack.dispatcher import dispatcher
 
-import blackjack.actions as actions
-import blackjack.result as result
-import random
-
 
 def main():
     deck = Deck()
     dealer = Dealer(deck, [])
     player = Player(deck, [])
 
+    # TODO: 雑多すぎる
     dhc = HandComponent(dispatcher, dealer, hole=True)
     phc = HandComponent(dispatcher, player, hole=False)
+    hsc = HitOrStandComponent(dispatcher)
     rsc = ResultComponent(dispatcher)
 
     game = Game(deck, dealer, player)
     game.begin()
-
     while not game.is_over():
         game.turn()
-        player.hit_or_stand()
-        if game.is_over():
-            dhc.hole = False
-            return game.end()
-        dealer.hit_or_stand()
-
     dhc.hole = False
     game.end()
-
-
 
 
 if __name__ == '__main__':
--- blackjack/game2.py
+++ blackjack/game2.py
@@ -3,6 +3,8 @@ import blackjack.result as result
 import blackjack.actions as actions
 
 
+# Stateを管理するGameStoreにするべきかも
+# どこまでをロジックにするか
 class Game:
     def __init__(self, deck, dealer, player):
         self.deck = deck
@@ -17,15 +19,26 @@ class Game:
         
     def turn(self):
         actions.show_hand()
+        # TODO: actionを経由する
+        self.player.hit_or_stand()
+        if self._should_end():
+            return
+        # TODO: actionを経由する
+        self.dealer.hit_or_stand()
     
     def end(self):
         res = result.judge_from_point(self.dealer.point, self.player.point)
         actions.game_over(res)
+    
+    def _should_end(self):
+        return (
+            self.dealer.point >= result.BLACKJACK
+            or self.player.point >= result.BLACKJACK
+        )
 
     def is_over(self):
         return (
             self.player.is_stand()
-            or self.dealer.point >= result.BLACKJACK
-            or self.player.point >= result.BLACKJACK
+            or self._should_end()
         )
     
