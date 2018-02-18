e7741139e3d4ce3b6510827f864fe212a7bc9da2
Ii Masamitsu <ii.masamitsu@gmail.com>
2018-02-18 14:04:24 +0900

    refactor: 属性を隠した

--- blackjack/game_store.py
+++ blackjack/game_store.py
@@ -5,13 +5,12 @@ from blackjack.dispatcher import dispatcher
 
 class GameStore:
     def __init__(self, deck, dealer, player):
-        #TODO: 隠す
-        self.deck = deck
-        self.dealer = dealer
-        self.player = player
-        self.game_result = None
-        self.game_is_over = False
-        self.turn_is_over = False
+        self._deck = deck
+        self._dealer = dealer
+        self._player = player
+        self._game_result = None
+        self._game_is_over = False
+        self._turn_is_over = False
         self._dispatcher = dispatcher
         dispatcher.on('BEGIN_GAME', self._begin_game)
         dispatcher.on('TURN_DEALER', self._turn_dealer)
@@ -21,20 +20,20 @@ class GameStore:
         #TODO: namedtupleでもいい
         #TODO: Gameでもいい
         return {
-            'deck': self.deck,
-            'dealer': self.dealer,
-            'player': self.player,
-            'game_result': self.game_result,
-            'game_is_over': self.game_is_over,
-            'turn_is_over': self.turn_is_over,
+            'deck': self._deck,
+            'dealer': self._dealer,
+            'player': self._player,
+            'game_result': self._game_result,
+            'game_is_over': self._game_is_over,
+            'turn_is_over': self._turn_is_over,
         }
     
     def _begin_game(self):
         #TODO: Gameに任せてもいい
-        random.shuffle(self.deck)
+        random.shuffle(self._deck)
         for _ in range(2):
-            self.player.hit()
-            self.dealer.hit()
+            self._player.hit()
+            self._dealer.hit()
         self._set_result()
         self._dispatcher.dispatch('CHANGE_STATE', state=self.get_state())
     
@@ -48,27 +47,27 @@ class GameStore:
     
     #TODO: 決定する処理はdealerに持たせる？
     def _turn_dealer(self):
-        if self.dealer.point < 17:
-            self.dealer.hit()
+        if self._dealer.point < 17:
+            self._dealer.hit()
         else:
-            self.dealer.stand()
+            self._dealer.stand()
         self._set_result()
         self._dispatcher.dispatch('CHANGE_STATE', state=self.get_state())
 
     #TODO: Gameに任せてもいい
     def _set_result(self):
-        self.turn_is_over = (
-            self.dealer.point >= 21
-            or self.player.point >= 21
+        self._turn_is_over = (
+            self._dealer.point >= 21
+            or self._player.point >= 21
         )
-        self.game_is_over = (
-            self.player.is_stand()
-            or self.turn_is_over
+        self._game_is_over = (
+            self._player.is_stand()
+            or self._turn_is_over
         )
-        if not self.game_is_over:
+        if not self._game_is_over:
             return
-        self.game_result = result.judge_from_point(
-            self.dealer.point,
-            self.player.point
+        self._game_result = result.judge_from_point(
+            self._dealer.point,
+            self._player.point
         )
     
