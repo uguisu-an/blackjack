a0e4523f0b8a5cae14c5f001962fd32a24591555
Ii Masamitsu <ii.masamitsu@gmail.com>
2018-02-18 13:30:26 +0900

    feat: ゲームの流れはGameRunnerに任せた

--- bj.py
+++ bj.py
@@ -2,6 +2,7 @@ import random
 #TODO: actionsにまとめる
 from blackjack.dispatcher import dispatcher
 from blackjack.game_store import GameStore
+from blackjack.game_runner import GameRunner
 from blackjack.game_component import GameComponent
 from blackjack.player_component import PlayerComponent
 from blackjack.dealer_component import DealerComponent
@@ -11,12 +12,9 @@ from blackjack.player import Dealer, Player
 
 
 class App:
-    def __init__(self):
-        self._deck = Deck()
-        self._dealer = Dealer(self._deck, [])
-        self._player = Player(self._deck, [])
-        #TODO: 中で初期化してもいい
-        self._store = GameStore(self._deck, self._dealer, self._player)
+    def __init__(self, store):
+        self._store = store
+        self._runner = GameRunner()
         self._components = [
             GameComponent(),
             PlayerComponent(),
@@ -24,23 +22,19 @@ class App:
         ]
         self.update(self._store.get_state())
     
-    def update(self, game):
-        self._game = game
+    def update(self, state):
+        self._state = state
         for component in self._components:
-            component.update(game)
+            component.update(self._state)
     
-    #TODO: ゲームの流れは誰が持つべきか？
     def run(self):
-        dispatcher.dispatch('BEGIN_GAME')
-        while not self._store.get_state()['game_is_over']:
-            dispatcher.dispatch('BEGIN_TURN')
-            dispatcher.dispatch('TURN_PLAYER')
-            if not self._store.get_state()['turn_is_over']:
-                dispatcher.dispatch('TURN_DEALER')
-            dispatcher.dispatch('END_TURN')
-        dispatcher.dispatch('END_GAME')
-
+        self._runner.run()
+    
 
 if __name__ == '__main__':
-    app = App()
+    deck = Deck()
+    dealer = Dealer(deck, [])
+    player = Player(deck, [])
+    store = GameStore(deck, dealer, player)
+    app = App(store)
     app.run()
---
+++ blackjack/game_runner.py
@@ -0,0 +1,23 @@
+from blackjack.dispatcher import dispatcher
+
+
+class GameRunner:
+    def __init__(self):
+        self._game_is_over = False
+        self._turn_is_over = False
+        self._dispatcher = dispatcher
+        self._dispatcher.on('CHANGE_STATE', self._change_state)
+
+    def run(self):
+        self._dispatcher.dispatch('BEGIN_GAME')
+        while not self._game_is_over:
+            self._dispatcher.dispatch('BEGIN_TURN')
+            self._dispatcher.dispatch('TURN_PLAYER')
+            if not self._turn_is_over:
+                self._dispatcher.dispatch('TURN_DEALER')
+            self._dispatcher.dispatch('END_TURN')
+        self._dispatcher.dispatch('END_GAME')
+    
+    def _change_state(self, state):
+        self._game_is_over = state['game_is_over']
+        self._turn_is_over = state['turn_is_over']
