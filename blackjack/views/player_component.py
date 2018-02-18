3ec50ef64610ff0968c60632df0af623709d3bab
Ii Masamitsu <ii.masamitsu@gmail.com>
2018-02-18 20:59:20 +0900

    refactor: 重複した処理を統合した

--- blackjack/views/player_component.py
+++ blackjack/views/player_component.py
@@ -7,24 +7,13 @@ class PlayerComponent:
         self._player = None
         self._dispatcher = dispatcher
         self._dispatcher.on('CHANGE_STATE', self.update)
-        self._dispatcher.on('BEGIN_TURN', self._begin_turn)
+        self._dispatcher.on('BEGIN_TURN', self._show_state)
         self._dispatcher.on('TURN_PLAYER', self._turn_player)
-        self._dispatcher.on('END_GAME', self._end_game)
+        self._dispatcher.on('END_GAME', self._show_state)
     
     def update(self, state={}):
         self._player = state.player
     
-    def _begin_turn(self):
-        self._show_name()
-        self._show_hand()
-        print()
-    
-    #TODO: beginと統合する？
-    def _end_game(self):
-        self._show_name()
-        self._show_hand()
-        print()
-
     def _turn_player(self):
         while True:
             print('Hit or Stand?')
@@ -39,10 +28,15 @@ class PlayerComponent:
                 break
             print('Use h[it] or s[tand].')
     
+    def _show_state(self):
+        self._show_name()
+        self._show_hand()
+        print()
+    
     def _show_name(self):
         print('{} ({}):'.format(self._player.name, self._player.point))
 
-    def _show_hand(self, hole=True):
+    def _show_hand(self):
         for card in self._player.hand:
             print('{} ({})'.format(card, pt.point(card.number)))
     
